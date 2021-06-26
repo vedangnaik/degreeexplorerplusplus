5af6ed6b43ecc100c8e131caa1578bfdd35dd3c8
Vedang Naik <vedang.a.naik@gmail.com>
2021-06-26 17:19:37 +0530

    Added testing code to check program requirements

--- src/Profile.js
+++ src/Profile.js
@@ -79,7 +79,8 @@ export class Profile extends HTMLDivElement {
     }
 
     TEMP() {
-        let courses = this.timetable.getScheduledCourses();
+        // let courses = this.timetable.getScheduledCourses();
+        let courses = {"CSC108H1": 1, "CSC148H1": 1, "CSC165H1": 1, "CSC207H1": 1, "CSC209H1": 1, "CSC236H1": 1, "CSC258H1": 1, "CSC263H1": 1, "CSC309H1": 1, "CSC311H1": 1, "CSC324H1": 1, "CSC336H1": 1, "CSC343H1": 1, "CSC367H1": 1, "CSC369H1": 1, "CSC373H1": 1, "CSC436H1": 1, "CSC443H1": 1, "CSC448H1": 1, "CSC456H1": 1, "CSC457H1": 1, "CSC458H1": 1, "CSC469H1": 1, "CSC488H1": 1, "GGR101H1": 1, "HPS390H1": 1, "HPS391H1": 1, "MAT137Y1": 1, "MAT223H1": 1, "MAT224H1": 1, "MAT237Y1": 1, "PCL102H1": 1, "PHL100Y1": 1, "PHY131H1": 1, "STA247H1": 1, "STA248H1": 1}
         for (let constraintFunc of ProgramData["ASSPE1689"]["completion"]) {
             if (!constraintFunc(courses)) {
                 console.log(constraintFunc);
