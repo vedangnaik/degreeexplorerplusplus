c19350fdf415be865f9fbbfdedf13dbbd8835a42
Vedang Naik <vedang.a.naik@gmail.com>
2021-07-08 18:47:31 +0530

    cleaned Console

--- src/Console.js
+++ src/Console.js
@@ -4,76 +4,75 @@ import { CourseTile } from "./CourseTile.js";
 import { GlobalTimetableID } from "./Timetable.js";
 import CourseData from "../resources/CourseData.js";
 
-
 export class Console extends HTMLDivElement {
-    static stylesheet = `
-        display: flex;
-        flex-direction: column;
-    `;
-
-    static searchButtonStylesheet = `
-        padding: 0.5vw; 
-        flex: 1;
-        font-size: 12px; 
-        outline: 1px solid grey;
-    `;
-
-    static evaluateProfileButtonStylesheet = `
-        border: 1px solid grey;
-        padding: 0.15vw;
-        border-radius: 5px;
-        background-color: gold;
-    `;
+    #courseSlot;
+    #searchInput;
 
     constructor() {
         super();
-        this.style = Console.stylesheet;
-            this.searchInput = document.createElement('input');
-            this.searchInput.style = "border: 1px solid black; border-radius: 5px;";
-        this.appendChild(this.searchInput);
+        this.style = `
+            display: flex;
+            flex-direction: column;
+        `;
+            this.#searchInput = document.createElement('input');
+            this.#searchInput.style = `
+                border: 1px solid black; 
+                border-radius: 5px;
+            `;
+        this.appendChild(this.#searchInput);
         this.appendChild(new Spacer({ "height": "0.5vw" }));
             const slotAndSearchDiv = document.createElement('div');
-            slotAndSearchDiv.style = "display: flex";
+            slotAndSearchDiv.style.display = "flex";
                 // Course slot to hold newly added courses. The bottom slot is deleted to prevent confusion.
-                this.courseSlot = new CourseSlotDiv();
-                this.courseSlot.removeChild(this.courseSlot.lowerSlot);
+                this.#courseSlot = new CourseSlotDiv();
+                this.#courseSlot.removeChild(this.#courseSlot.lowerSlot);
                 // The top slot's dragover and drop handlers are deleted to prevent the box being used as a storage for course tiles and potentially messing up the prerequisite data.
-                this.courseSlot.upperSlot.ondragover = () => {};
-                this.courseSlot.upperSlot.ondrop  = () => {};
+                this.#courseSlot.upperSlot.ondragover = () => {};
+                this.#courseSlot.upperSlot.ondrop  = () => {};
                 
-                this.searchButton = document.createElement('button');
-                this.searchButton.innerText = "Add Courses & PoSTs";
-                this.searchButton.onclick = this.searchCourse.bind(this);
-                this.searchButton.style = Console.searchButtonStylesheet;
-            slotAndSearchDiv.appendChild(this.courseSlot);
+                const searchButton = document.createElement('button');
+                searchButton.innerText = "Add Courses & PoSTs";
+                searchButton.onclick = this.#searchCourse.bind(this);
+                searchButton.style = `
+                    padding: 0.5vw; 
+                    flex: 1;
+                    font-size: 12px; 
+                    outline: 1px solid grey;
+                `;
+            slotAndSearchDiv.appendChild(this.#courseSlot);
             slotAndSearchDiv.appendChild(new Spacer({ "width": "0.5vw" }));
-            slotAndSearchDiv.appendChild(this.searchButton);
+            slotAndSearchDiv.appendChild(searchButton);
         this.appendChild(slotAndSearchDiv);
         this.appendChild(new Spacer({ "height": "0.5vw" }));
-            this.evaluateProfileButton = document.createElement('button');
-            this.evaluateProfileButton.innerText = "EVALUATE PROFILE";
-            this.evaluateProfileButton.style = Console.evaluateProfileButtonStylesheet;
-            this.evaluateProfileButton.onclick = this.evaluateProfile.bind(this);
-        this.appendChild(this.evaluateProfileButton);
+            const evaluateProfileButton = document.createElement('button');
+            evaluateProfileButton.innerText = "EVALUATE PROFILE";
+            evaluateProfileButton.style = `
+                border: 1px solid grey;
+                padding: 0.15vw;
+                border-radius: 5px;
+                background-color: gold;
+            `;
+            evaluateProfileButton.onclick = this.#evaluateProfile.bind(this);
+        this.appendChild(evaluateProfileButton);
     }
 
-    searchCourse() {
-        const id = this.searchInput.value;
+    #searchCourse() {
+        const id = this.#searchInput.value;
         if (CourseData[id]) {
             // Delete any existing tiles in the slot, then add the new child
-            this.courseSlot.upperSlot.replaceChildren();
-            this.courseSlot.upperSlot.appendChild(new CourseTile(id));
+            this.#courseSlot.upperSlot.replaceChildren();
+            this.#courseSlot.upperSlot.appendChild(new CourseTile(id));
         } else {
             // Flash the slot red for a second to indicate an error
             // TODO: Maybe change this into a smooth fadein/fadeout
-            this.courseSlot.style.backgroundColor = "#ff8080";
+            this.#courseSlot.style.backgroundColor = "#ff8080";
             setTimeout(() => {
-               this.courseSlot.style.backgroundColor = "transparent"; 
+               this.#courseSlot.style.backgroundColor = "transparent"; 
             }, 1000);
         }
     }
 
-    evaluateProfile() {
+    #evaluateProfile() {
         const scheduledCourses = document.getElementById(GlobalTimetableID).getTimetableJSON()["scheduledCourses"];
         const scheduledPrograms = {};
 
