3c6637d655d6020699aa72d22d3f3d007ba0a9d2
Vedang Naik <vedang.a.naik@gmail.com>
2021-08-08 20:21:27 +0000

    Fixed .gitignore to include missing file

---
+++ resources/__exports__.js
@@ -0,0 +1,3 @@
+export { default as CourseData } from "./CourseData.js";
+export { default as ProgramData } from "./ProgramData.js";
+export { default as CourseCategoriesData } from "./CourseCategoriesData.js";
