6c29f6ec3f1fee9200138f8e7cc855c7f01b8026
Vedang Naik <vedang.a.naik@gmail.com>
2021-08-21 20:54:20 +0530

    Added starter highlighting methods to various parts of DE++

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import "./src/ui/exports.js"
 // Specific imports this main needs for the final setup
 import { GLOBAL_COURSE_INFO_PANEL_ID, GLOBAL_COURSE_SCHEDULE_ID, GLOBAL_PROGRAM_SCHEDULE_ID } from "./src/Constants.js";
 
-function main() {
+function connectObservers() {
     // Start observing the timetable for changes here - tell the timetable itself and the course panel if something changes
     const courseScheduleInstance = document.getElementById(GLOBAL_COURSE_SCHEDULE_ID);
     const coursePanelInstance = document.getElementById(GLOBAL_COURSE_INFO_PANEL_ID);
@@ -15,11 +15,29 @@ function main() {
         programScheduleInstance.resetPrograms();
     });
     timetableObserver.observe(courseScheduleInstance, { childList: true, subtree: true });
+}
 
+function createNewProfile() {
     // This link clicks the 'New Profile' button to create a new profile for the user to use.
     document.querySelector("body > div > div:nth-child(5) > div:nth-child(3) > div:nth-child(1) > button").click()
     // This line clicks the radio button of the new profile that was just created.
     document.querySelector("body > div > div:nth-child(5) > div:nth-child(3) > div:nth-child(3) > div > label").click();
 }
 
-window.addEventListener('DOMContentLoaded', main);
+function connectTutorialHighlights() {
+    function attachSpanToElementForHighlight(span, element) {
+        span.onmouseenter = () => { element.style.outline = "5px solid yellow"; }
+        span.onmouseleave = () => { element.style.outline = "revert"; }
+    }
+
+    // For the Profile section
+    attachSpanToElementForHighlight(document.getElementById("profile-highlight"), document.querySelector("body > div > div:nth-child(3)"));
+    // For the Course Schedule
+    attachSpanToElementForHighlight(document.getElementById("cs-highlight"), document.getElementById(GLOBAL_COURSE_SCHEDULE_ID));
+    // For the Program Schedule
+    attachSpanToElementForHighlight(document.getElementById("ps-highlight"), document.getElementById(GLOBAL_PROGRAM_SCHEDULE_ID));
+}
+
+window.addEventListener('DOMContentLoaded', connectObservers);
+window.addEventListener('DOMContentLoaded', createNewProfile);
+window.addEventListener('DOMContentLoaded', connectTutorialHighlights);
